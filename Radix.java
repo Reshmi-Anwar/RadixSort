0997476aaf4035cd44c323387d20b327c22d2f8a
Reshmi <mia.anw21@gmail.com>
2021-01-12 12:18:30 -0500

    used class tester cases to finish nth and length methods

--- Radix.java
+++ Radix.java
@@ -1,10 +1,31 @@
 public class Radix{
   public static int nth(int n, int col){
     int x = ((n / (int)Math.pow(10, col)) % 10);
-    return x;
+    return Math.abs(x);
   }
 
   public static int length(int n){
-    
+    if (n==0){
+      return 1;
+    }
+    if (n < 0){
+      n = Math.abs(n);
+    }
+    int lengthNum = (int) Math.log10(n) + 1;
+    return lengthNum;
+  }
+
+  //simple tester cases from class notes
+  public static void main(String args[]){
+    System.out.println("Nth");
+    System.out.println(nth(123, 1));
+    System.out.println(nth(-123, 1));
+    System.out.println(nth(123, 2));
+    System.out.println(nth(-123, 2));
+    System.out.println("Length");
+    System.out.println(length(0));
+    System.out.println(length(15));
+    System.out.println(length(-10));
+    System.out.println(length(4759));
   }
 }
