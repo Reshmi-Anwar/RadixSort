150d164cb39b91d56327d5a05027982998913909
Reshmi <mia.anw21@gmail.com>
2021-01-13 15:31:11 -0500

    made bulk of simple sort: just need to add how many passes it goes through. while or do-while loop?

--- Radix.java
+++ Radix.java
@@ -16,7 +16,8 @@ public class Radix{
   }
 
   //simple tester cases from class notes
-  /*public static void main(String args[]){
+  /*
+  public static void main(String args[]){
     System.out.println("Nth");
     System.out.println(nth(123, 1));
     System.out.println(nth(-123, 1));
@@ -30,9 +31,68 @@ public class Radix{
   }
   */
 
+
   public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
     for (SortableLinkedList bucket : buckets){
       original.extend(bucket);
     }
   }
+
+/*
+  public static int largestDigits(SortableLinkedList original){
+    int largestValue = original.get(0);
+    for (int i = 0; i < original.size() - 1; i++){
+      if (original.get(i) > largestValue){
+        largestValue = original.get(i);
+      }
+    }
+    int digitLargest = length(largestValue);
+    return digitLargest;
+  }
+
+
+  public static void main(String args[]){
+    SortableLinkedList myList = new SortableLinkedList();
+    myList.add(2);
+    myList.add(52);
+    myList.add(4);
+    myList.add(7);
+    System.out.println(largestDigits(myList));
+  }
+  */
+
+
+
+
+
+
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    int numDigit = -1;
+    int digitPlace = 0; //starts checking at ones place
+    for(int i = 0; i < 10; i++){ //creates buckets 0 - 9
+      buckets[i] = new SortableLinkedList();
+    }
+    while (data.size() > 0){
+      int shift = data.get(0);
+      data.remove(0);
+      if (numDigit < length(shift)){
+        numDigit = length(shift); //establishes the number of passes based on the greatest num of digits
+        buckets[nth(shift, digitPlace)].add(shift);
+      }
+    }
+    merge(data, buckets);
+    digitPlace ++;
+
+
+    /*
+    int digitPlace = 0;
+    while(data.size() > 0){
+      int iterativeValue = data.remove(0);
+      if
+    }
+    */
+
+  }
+
 }
